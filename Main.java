907600232d189135e944d8a7b8898708f771d9f6
kirivanCode <ivansierravega8@gmail.com>
2025-04-03 07:26:02 -0500

    feat: cambio a joptionPane

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 import java.util.List;
 
@@ -5,24 +6,28 @@ public class Main {
     public static void main(String[] args) {
         GestorRecetas gestor = new GestorRecetas();
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Ingrese el nombre de la receta:");
-        String nombre = scanner.nextLine();
-        
-        System.out.println("Ingrese los ingredientes (separados por coma):");
-        String[] ingredientes = scanner.nextLine().split(",");
-        
-        System.out.println("Ingrese las instrucciones:");
-        String instrucciones = scanner.nextLine();
-        
+
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre de la receta:");
+
+        String ingredientesInput = JOptionPane.showInputDialog("Ingrese los ingredientes (separados por coma):");
+        String[] ingredientes = ingredientesInput.split(",");
+
+        String instrucciones = JOptionPane.showInputDialog("Ingrese las instrucciones:");
+
         Receta nuevaReceta = new Receta(nombre, ingredientes, instrucciones);
         gestor.agregarReceta(nuevaReceta);
-        
-        System.out.println("Receta agregada: " + nuevaReceta.getNombre());
 
-        System.out.println("\nLista de recetas guardadas:");
-        for (Receta receta : gestor.obtenerRecetas()) {
-            System.out.println("- " + receta.getNombre());
+        JOptionPane.showMessageDialog(null, "Receta agregada: " + nuevaReceta.getNombre());
+
+        // Construimos la lista de recetas en un String
+        List<Receta> recetas = gestor.obtenerRecetas();
+        StringBuilder listaRecetas = new StringBuilder("Recetas guardadas:\n");
+
+        for (Receta receta : recetas) {
+            listaRecetas.append("- ").append(receta.getNombre()).append("\n");
         }
+
+        // Mostramos las recetas guardadas en un JOptionPane
+        JOptionPane.showMessageDialog(null, listaRecetas.toString(), "Lista de Recetas", JOptionPane.INFORMATION_MESSAGE);
     }
 }
